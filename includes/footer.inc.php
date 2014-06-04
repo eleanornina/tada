b8c254f76f51bd74463392465e9ed7506593aa44
Jlamx93 <jlamx93gmail.com>
2014-06-03 21:52:14 -0700

    i deleted last list item.

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -7,8 +7,6 @@
                         </li>-->
                         <li><i class="fa fa-twitter fa-3x"></i>
                         </li>
-                      <!--  <li><i class="fa fa-dribbble fa-3x"></i>
-                        </li> -->
                     </ul> 
                     
                <p class="footer">Copyright &copy; Company 2013</p>
