9d1b25596274a8aa6015db370d1ce3a22b652636
melsm650 <melsm13650@gmail.com>
2014-06-03 21:44:58 -0700

    commented out first list

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -3,8 +3,8 @@
             <div class="row">
                 <div class="col-md-6 col-md-offset-3 text-center">
                     <ul class="list-inline">
-                        <li><i class="fa fa-facebook fa-3x"></i>
-                        </li>
+                        <!--<li><i class="fa fa-facebook fa-3x"></i>
+                        </li>-->
                         <li><i class="fa fa-twitter fa-3x"></i>
                         </li>
                         <li><i class="fa fa-dribbble fa-3x"></i>
