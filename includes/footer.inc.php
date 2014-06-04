34326e3300868852ec0d1434a4d17a7bac4fed70
Jlamx93 <jlamx93gmail.com>
2014-06-03 21:39:08 -0700

    I commented out the footer list item

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -7,9 +7,9 @@
                         </li>
                         <li><i class="fa fa-twitter fa-3x"></i>
                         </li>
-                        <li><i class="fa fa-dribbble fa-3x"></i>
-                        </li>
-                    </ul>  
+                      <!--  <li><i class="fa fa-dribbble fa-3x"></i>
+                        </li> -->
+                    </ul> 
                     
                <p class="footer">Copyright &copy; Company 2013</p>
                 </div>
