0ce6e4276ff7e9040a65b9c9115c7e8e4b373775
melsm650 <melsm13650@gmail.com>
2014-06-03 21:50:54 -0700

    i uncommented list

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -3,12 +3,12 @@
             <div class="row">
                 <div class="col-md-6 col-md-offset-3 text-center">
                     <ul class="list-inline">
-                        <!--<li><i class="fa fa-facebook fa-3x"></i>
-                        </li>-->
+                        <li><i class="fa fa-facebook fa-3x"></i>
+                        </li>
                         <li><i class="fa fa-twitter fa-3x"></i>
                         </li>
-                      <!--  <li><i class="fa fa-dribbble fa-3x"></i>
-                        </li> -->
+                      <li><i class="fa fa-dribbble fa-3x"></i>
+                        </li>
                     </ul> 
                     
                <p class="footer">Copyright &copy; Company 2013</p>
